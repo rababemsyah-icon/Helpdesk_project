cd3199f603bf48931f7c3d79dac961ee86913735
Ra babe <rababe.msyah@usmba.ac.ma>
2026-09-01 15:01:59 +0000

    Nettoyage complet pour déploiement SQLite

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'votre-cle-secrete-par-defaut'
-    
-    # Remplacez la ligne ci-dessous : 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    
-    # Par celle-ci (utilise SQLite par défaut si la variable n'existe pas)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///helpdesk.db'
-    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///helpdesk.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
